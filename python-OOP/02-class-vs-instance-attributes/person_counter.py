dc1091f381cffec610d83ad8e2a115fc3665c057
Shaw Nguyen <nguyenvanthanhtung2001@gmail.com>
2025-05-31 22:00:55 +0700

    Update __del__

--- python-OOP/02-class-vs-instance-attributes/person_counter.py
+++ python-OOP/02-class-vs-instance-attributes/person_counter.py
@@ -8,6 +8,10 @@ class Person:
         self.name = name
         Person.total_people += 1
 
+    def __del__(self):
+        Person.total_people -= 1
+        print(f"{self.name} has been removed from the count.")
+
     @classmethod
     def display_total_people(cls):
         print("Total number of people:", cls.total_people)
@@ -16,5 +20,6 @@ class Person:
 person1 = Person("Alice")
 person2 = Person("Bob")
 person3 = Person("Charlie")
+del person2  # Deleting an instance does not affect the class attribute
 
 Person.display_total_people()
